1616784dc9a2a78f35feb4467c0d84d3a67d9a47
Vsp003 <Fahad@vsple.com>
2024-07-10 17:40:32 +0530

    TodayFinal

--- src/Pages/Front/Category/StoreSubCategory.jsx
+++ src/Pages/Front/Category/StoreSubCategory.jsx
@@ -4,7 +4,6 @@ import { responsiveFontSize, responsiveHeight, responsiveWidth } from "react-nat
 import Header from "../../../comon/HeaderFooter/Header";
 import style from "../../../utils/css/comoncss/style";
 import Addas from "../../../comon/Ads/Ads";
-import CategryCard from "./CategoryCard";
 import { getData } from "../../../Apis/ListsApi/ListPostApi";
 import ActiveEndicator from "../../../comon/Loader/ActiveEndicator";
 import { useNavigation } from "@react-navigation/native";
@@ -14,14 +13,20 @@ const SubCategory = (props) => {
     const [dataa, setData] = useState([]);
     const navigation = useNavigation();
     const [slug, setSlug] = useState(props?.route?.params?.slug);
-  console.warn('Here',slug)
+    console.warn('Here', slug)
 
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const data = await getData(`store/${slug}`, { limit: 20, offset: 0 });
-                setData(data?.data?.list ? data?.data?.list : undefined);
+
+                if (data?.status == false) {
+                    var product = await getData(`store/getProducts`, { limit: 9, offset: 0 })
+                    setData(product?.data?.list)
+                } else {
+                    setData(data?.data?.list ? data?.data?.list : undefined);
+                }
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -40,7 +45,7 @@ const SubCategory = (props) => {
 
     return (
         <>
-            <Header top={<Text>{slug}{'\n'}</Text>} />
+            <Header top={<Text>{slug?.charAt(0).toUpperCase() + slug?.slice(1)}{'\n'}</Text>} />
 
             <FlatList
                 style={[style.bgwhite, {
@@ -51,7 +56,7 @@ const SubCategory = (props) => {
                 renderItem={renderCategory}
                 keyExtractor={(item, index) => `${item.id}_${index}`}
                 ListHeaderComponent={<Addas />}
-                ListEmptyComponent={<View style={{paddingVertical:responsiveHeight(5)}}>{dataa === undefined ? <Text style={[style.comoncolor2,{textAlign:'center',fontSize:responsiveFontSize(1.7),paddingVertical:responsiveHeight(10)}]}>No Products Found</Text>  :<ActiveEndicator />}</View>}
+                ListEmptyComponent={<View style={{ paddingVertical: responsiveHeight(5) }}>{dataa === undefined ? <Text style={[style.comoncolor2, { textAlign: 'center', fontSize: responsiveFontSize(1.7), paddingVertical: responsiveHeight(10) }]}>No Products Found</Text> : <ActiveEndicator />}</View>}
                 numColumns={2} // Adjust as needed based on your design
             />
         </>
